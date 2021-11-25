d21350e7d6e61d3b39e5514fd61476bc260f9051
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-26 00:00:51 +0600

    Song dispaly with artist name

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const searchSongs = () => {
 const displaySongs = (songs) => {
   const songContainer = document.getElementById("song-container");
   songs.forEach((song) => {
+    console.log(songs);
     const songDiv = document.createElement("div");
-    songDiv.className = "search-result col-md-8 mx-auto py-4";
+    songDiv.className = "single-result row align-items-center my-3 p-3";
     songDiv.innerHTML = `
             <div class="col-md-9">
-              <h3 class="lyrics-name">Purple Noon</h3>
-              <p class="author lead">Album by <span>Washed Out</span></p>
+              <h3 class="lyrics-name">${song.title}</h3>
+              <p class="author lead">Album by <span>${song.artist.name}</span></p>
             </div>
             <div class="col-md-3 text-md-right text-center">
               <button class="btn btn-success">Get Lyrics</button>
