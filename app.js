4dc670bbade7309ed7a41db368ad85add2d98da4
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-12-18 12:46:35 +0600

    Spinner added

--- app.js
+++ app.js
@@ -6,8 +6,14 @@
 //   displaySongs(data.data);
 // };
 const searchSongs = () => {
+  const lyricsGet = document.getElementById("song-lyrics");
+  lyricsGet.innerText = "";
+  const errorTag = document.getElementById("error-message");
+  console.log(errorTag, "Hello");
+  errorTag.innerText = "";
   const searchText = document.getElementById("search-field").value;
   const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+  toggleSpinner();
   fetch(url)
     .then((res) => res.json())
     .then((data) => displaySongs(data.data))
@@ -18,6 +24,7 @@ const searchSongs = () => {
 
 const displaySongs = (songs) => {
   const songContainer = document.getElementById("song-container");
+
   songContainer.innerHTML = "";
   songs.forEach((song) => {
     //console.log(songs);
@@ -25,19 +32,23 @@ const displaySongs = (songs) => {
     songDiv.className = "single-result row align-items-center my-3 p-3";
     songDiv.innerHTML = `
             <div class="col-md-9">
-              <h3 class="lyrics-name">${song.title}</h3>
+                              <h3 class="lyrics-name">${song.title}</h3>
               <p class="author lead">Album by <span>${song.artist.name}</span></p>
-               <audio controls>
-                  <source src="${song.preview}" type="audio/mpeg">
+              <audio controls>
+                 
+              
+              
+              <source src="${song.preview}" type="audio/mpeg">
             </audio>
+               
               </div>
-           
             <div class="col-md-3 text-md-right text-center">
               <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Lyrics</button>
             </div>
             
     `;
     songContainer.appendChild(songDiv);
+    toggleSpinner(false);
   });
 };
 
@@ -69,4 +80,11 @@ const displayLyrics = (lyrics) => {
 const displayError = (error) => {
   const errorTag = document.getElementById("error-message");
   errorTag.innerText = error;
+};
+
+const toggleSpinner = () => {
+  const spinner = document.getElementById("loadingSpinner");
+  const songs = document.getElementById("song-container");
+  spinner.classList.toggle("d-none");
+  songs.classList.toggle("d-none");
 };
